eb84af2b38d1d4c7f2e6e523840ff108b8a252ab
vladi <vladi@LAPTOP-QRGAGD2K>
2022-10-09 23:27:27 +0500

    Add project files.

---
+++ Lesson3/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Collections.Generic;
+
+namespace Lesson3
+{
+    class Human
+    {
+        private static int Age;
+        public static string Name { get; set; }
+    }
+    class Student : Human
+    {
+        public static int Group { get; set; }
+    }
+    class Group
+    {
+        List<Student> student { get; set; }
+        public static string Faculty { get; set; }
+    }
+}
---
+++ Lesson3/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Lesson3
+{
+    static class Program
+    {
+        static void Main()
+        {
+            string[] text = File.ReadAllLines("..\\..\\..\\text.txt");
+            Console.WriteLine(CreateText(text));
+        }
+
+        public static string GetClass(string[] text)
+        {
+            return text[0].Remove(0, 7);
+        }
+
+        public static string[] GetProperties(string text)
+        {
+            string[] all = text.Split(':');
+            string[] properties = all[1].Split(',');
+            return properties;
+        }
+
+        public static StringBuilder CreateText(string[] text)
+        {
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("namespace SillyProgram");
+            sb.AppendLine("{");
+            sb.AppendLine($"\tclass {GetClass(text)}");
+            sb.AppendLine("\t{");
+
+            foreach (string line in GetProperties(text[1]))
+            {
+                string[] props = line.Split(' ');
+                if (props.Length == 4)
+                {
+                    sb.Append("\t\tprivate ");
+                    switch (props[2])
+                    {
+                        case "число":
+                            sb.Append("int ");
+                            break;
+                        case "строка":
+                            sb.Append("string ");
+                            break;
+                        case "символ":
+                            sb.Append("char ");
+                            break;
+                        default:
+                            sb.Append("bool ");
+                            break;
+                    }
+                    sb.AppendLine(props[3] + ";");
+                }
+
+                else
+                {
+                    sb.Append("\t\tpublic ");
+                    switch (props[1])
+                    {
+                        case "число":
+                            sb.Append("int ");
+                            break;
+                        case "строка":
+                            sb.Append("string ");
+                            break;
+                        case "символ":
+                            sb.Append("char ");
+                            break;
+                        default:
+                            sb.Append("bool ");
+                            break;
+                    }
+                    sb.AppendLine(props[2] + " { get; set; }");
+                }
+            }
+
+            sb.AppendLine("\t}");
+            sb.AppendLine("}");
+            return sb;
+        }
+    }
+}
+
+
